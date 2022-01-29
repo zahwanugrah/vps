f9aa5d6da05b3ddb655799fe3ba42814c32efbba
zeaking <85456530+zeaking@users.noreply.github.com>
2022-01-29 23:18:26 +0700

    Update about.sh

--- ssh/about.sh
+++ ssh/about.sh
@@ -2,12 +2,12 @@
 
 clear
 echo -e "================================================="
-echo -e "#     Premium Auto Script By Akbar Maulana      #"
+echo -e "#     Premium Auto Script By ZEAKING STORE      #"
 echo -e "================================================="
 echo -e "# For Debian 10 64 bit                          #"
 echo -e "# For Ubuntu 18.04 & Ubuntu 20.04 64 bit        #"
 echo -e "# For VPS with KVM and VMWare virtualization    #"
-echo -e "# Build Up By Akbar Maulana                     #"
+echo -e "# Build Up By ZEAKING                     #"
 echo -e "================================================="
 echo -e "# Thanks To                                     #"
 echo -e "================================================="
