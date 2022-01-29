1f17e71211bd35e91d2b651ba318bd88a3eedd12
zeaking <85456530+zeaking@users.noreply.github.com>
2022-01-30 05:49:11 +0700

    Update limitspeed.sh

--- backup/limitspeed.sh
+++ backup/limitspeed.sh
@@ -12,9 +12,8 @@ CYAN='\033[0;36m'
 LIGHT='\033[0;37m'
 # ==========================================
 # Getting
-MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
-IZIN=$( curl https://raw.githubusercontent.com/zeaking/izin/main/ip.txt | grep $MYIP )
+IZIN=$( curl https://raw.githubusercontent.com/zeaking/vps/master/ipvps.txt | grep $MYIP )
 if [ $MYIP = $IZIN ]; then
 echo -e "${NC}${GREEN}Permission Accepted...${NC}"
 else
