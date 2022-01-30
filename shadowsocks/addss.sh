af7baf0c43f6d1ca060b59a32a4e241db519a076
zeaking <85456530+zeaking@users.noreply.github.com>
2022-01-30 14:56:55 +0700

    Update addss.sh

--- shadowsocks/addss.sh
+++ shadowsocks/addss.sh
@@ -14,7 +14,7 @@ LIGHT='\033[0;37m'
 # Getting
 MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
-IZIN=$( curl https://raw.githubusercontent.com/zeaking/izin/main/ip.txt | grep $MYIP )
+IZIN=$( curl https://raw.githubusercontent.com/zeaking/vps/master/ipvps.txt | grep $MYIP )
 if [ $MYIP = $IZIN ]; then
 echo -e "${NC}${GREEN}Permission Accepted...${NC}"
 else
@@ -120,4 +120,4 @@ echo -e "Link TLS    : $linkss1"
 echo -e "========================="
 echo -e "Link No TLS : $linkss2"
 echo -e "========================="
-echo -e "Script By Akbar Maulana"
+echo -e "Script By ZEAKING STORE"
