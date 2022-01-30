40f013312808ae48a501369be261e269aa2066b9
zeaking <85456530+zeaking@users.noreply.github.com>
2022-01-30 15:07:08 +0700

    Update addtrgo.sh

--- trojango/addtrgo.sh
+++ trojango/addtrgo.sh
@@ -14,7 +14,7 @@ LIGHT='\033[0;37m'
 # Getting
 MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
-IZIN=$( curl https://raw.githubusercontent.com/zeaking/izin/main/ip.txt | grep $MYIP )
+IZIN=$( curl https://raw.githubusercontent.com/zeaking/vps/master/ipvps.txt | grep $MYIP )
 if [ $MYIP = $IZIN ]; then
 echo -e "${NC}${GREEN}Permission Accepted...${NC}"
 else
@@ -64,4 +64,4 @@ echo -e "Expired    : $exp"
 echo -e "========================="
 echo -e "Link TrGo  : ${link}"
 echo -e "========================="
-echo -e "Script By Akbar Maulana"
+echo -e "Script By ZEAKING STORE"
