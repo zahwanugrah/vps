4f5184b0086ef9c7dde85b499c433598becde304
zeaking <85456530+zeaking@users.noreply.github.com>
2022-01-30 15:15:29 +0700

    Update addtrojan.sh

--- xray/addtrojan.sh
+++ xray/addtrojan.sh
@@ -14,7 +14,7 @@ LIGHT='\033[0;37m'
 # Getting
 MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
-IZIN=$( curl https://raw.githubusercontent.com/zeaking/izin/main/ip.txt | grep $MYIP )
+IZIN=$( curl https://raw.githubusercontent.com/zeaking/vps/master/ipvps.txt | grep $MYIP )
 if [ $MYIP = $IZIN ]; then
 echo -e "${NC}${GREEN}Permission Accepted...${NC}"
 else
@@ -62,4 +62,4 @@ echo -e "Expired  : $exp"
 echo -e "=========================="
 echo -e "Link TR  : ${trojanlink}"
 echo -e "=========================="
-echo -e "Script By Akbar Maulana"
+echo -e "Script By ZEAKING STORE"
