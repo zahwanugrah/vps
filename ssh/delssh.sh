8334e8079774e104d6409178f51f3f32f945075b
zahwanugrah <39833642+zahwanugrah@users.noreply.github.com>
2022-03-06 21:41:14 +0700

    Update delssh.sh

--- ssh/delssh.sh
+++ ssh/delssh.sh
@@ -14,15 +14,7 @@ LIGHT='\033[0;37m'
 # Getting
 MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
-IZIN=$( curl https://raw.githubusercontent.com/zeaking/vps/master/ipvps.txt | grep $MYIP )
-if [ $MYIP = $IZIN ]; then
-echo -e "${NC}${GREEN}Permission Accepted...${NC}"
-else
-echo -e "${NC}${RED}Permission Denied!${NC}";
-echo -e "${NC}${LIGHT}Please Contact Admin!!"
-echo -e "${NC}${LIGHT}Telegram : https://t.me/zeaking5"
-exit 0
-fi
+
 read -p "Username SSH to Delete : " Pengguna
 
 if getent passwd $Pengguna > /dev/null 2>&1; then
