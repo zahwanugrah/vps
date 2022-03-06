e4ef0060533b0ce38d9d449213b1d6818c7be07c
zahwanugrah <39833642+zahwanugrah@users.noreply.github.com>
2022-03-06 21:33:50 +0700

    Update about.sh

--- ssh/about.sh
+++ ssh/about.sh
@@ -2,7 +2,7 @@
 
 clear
 echo -e "================================================="
-echo -e "#     Premium Auto Script By ZEAKING STORE      #"
+echo -e "#       Premium Auto Script                     #"
 echo -e "================================================="
 echo -e "# For Debian 10 64 bit                          #"
 echo -e "# For Ubuntu 18.04 & Ubuntu 20.04 64 bit        #"
