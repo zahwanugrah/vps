0c443d110f7cb29727170bb8294737d11903b39b
zahwanugrah <39833642+zahwanugrah@users.noreply.github.com>
2022-03-06 21:32:15 +0700

    Update backup.sh

--- backup/backup.sh
+++ backup/backup.sh
@@ -13,15 +13,7 @@ LIGHT='\033[0;37m'
 # ==========================================
 # Getting
 echo "Checking VPS"
-IZIN=$( curl https://raw.githubusercontent.com/zeaking/vps/master/ipvps.txt | grep $MYIP )
-if [ $MYIP = $IZIN ]; then
-echo -e "${NC}${GREEN}Permission Accepted...${NC}"
-else
-echo -e "${NC}${RED}Permission Denied!${NC}";
-echo -e "${NC}${LIGHT}Please Contact Admin!!"
-echo -e "${NC}${LIGHT}Telegram : https://t.me/zeaking5"
-exit 0
-fi
+
 clear
 IP=$(wget -qO- ipinfo.io/ip);
 date=$(date +"%Y-%m-%d")
